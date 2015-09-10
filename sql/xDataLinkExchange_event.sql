58a80beb0bb27da5767cbca07b7f9b82c0e34ed1
DeadCat93 <alexey.i.panshin@gmail.com>
2015-09-10 14:55:44 +0300

    xDataLinkExchange_event

---
+++ sql/xDataLinkExchange_event.sql
@@ -0,0 +1,19 @@
+sa_make_object 'event', 'xDataLinkExchange', 'bp'
+;
+alter event bp.xDataLinkExchange
+add schedule xDataLinkExchange
+start time '08:00' on  ('Mon','Tue','Wed','Thu','Fri','Sat','Sun')
+handler
+begin
+
+    call bp.xDataLinkExchange(today() -1, 1, 1, 1);
+
+    exception
+    when others then
+
+        call util.errorHandler('bp.xDataLinkExchange', SQLSTATE, errormsg());
+
+        rollback;
+
+end
+;
